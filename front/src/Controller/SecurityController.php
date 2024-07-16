16b519d22e3b3399028bca548851a7f61e844d44
Stanislas Bertreux <bertreux.stanislas@gmail.com>
2024-07-16 10:34:24 +0200

    Ajout connexion
    Ajout d'une page app_login qui va tester l'email et le mot de passe en hasher pour tester de connecter l'utilisateur
    
    AJout d'une page de register pour créer de nouveaux utilisateur
    
    Ajout d'une page de changement de mot de passe
    
    Ajout du champs is_verified (si l'utilisateur est aller tester son mail)

--- front/src/Controller/SecurityController.php
+++ front/src/Controller/SecurityController.php
@@ -10,29 +10,25 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends FrontAbstractController
 {
-    #[Route(path: '/connexion', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+#[Route(path: '/login', name: 'app_login')]
+public function login(AuthenticationUtils $authenticationUtils): Response
+{
+    // if ($this->getUser()) {
+    //     return $this->redirectToRoute('target_path');
+    // }
 
-        return $this->render('security/login.html.twig', 
-        [
-        'last_username' => $lastUsername, 
-        'error' => $error,
-        'lsession' => $request->getSession()->get('cart'),
-    ]);
-    }
+    // get the login error if there is one
+    $error = $authenticationUtils->getLastAuthenticationError();
+    // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
+    return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+}
+
+#[Route(path: '/logout', name: 'app_logout')]
+public function logout(): void
+{
+    throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+}
 }
